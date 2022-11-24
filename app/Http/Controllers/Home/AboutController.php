fc13af57336ed527bb727750a37b63884af4b597
Danylo <danilo.savchenko96@gmail.com>
2022-11-24 21:19:22 +0200

    Multi Image components

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\MultiImage;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -52,5 +54,30 @@ class AboutController extends Controller
     {
         $aboutPage = About::find(1);
         return view('frontend.about_page', compact('aboutPage'));
+    }
+
+    public function AboutMultiImage()
+    {
+        return view('admin.about_page.multimage');
+    }
+
+    public function StoreMultiImage(Request $request)
+    {
+        $image = $request->file('multi_image');
+
+        foreach ($image as $multi_image) {
+            $name_gen = hexdec(uniqid()) . '.' . $multi_image->getClientOriginalExtension();
+            Image::make($multi_image)->resize(220, 220)->save('upload/multi/' . $name_gen);
+            $save_url = 'upload/multi/' . $name_gen;
+            MultiImage::insert([
+                'multi_image' => $save_url,
+                'created_at' => Carbon::now()
+            ]);
+            $notification = array(
+                'message' => 'Multi Image Inserted Successfully',
+                'alert-type' => 'success'
+            );
+        }
+        return redirect()->back()->with($notification);
     }
 }
