c4ea96ca253b683f7972f024ca8c732dcc73a381
Danylo <danilo.savchenko96@gmail.com>
2022-11-25 14:25:12 +0200

    Multi Image components

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -80,4 +80,55 @@ class AboutController extends Controller
         }
         return redirect()->back()->with($notification);
     }
+
+    public function AllMultiImage()
+    {
+        $allMultiImage = MultiImage::all();
+
+        return view('admin.about_page.all_multimage', compact('allMultiImage'));
+    }
+
+    public function EditMultiImage($id)
+    {
+        $multiImage = MultiImage::findOrFail($id);
+
+        return view('admin.about_page.edit_multi_image', compact('multiImage'));
+    }
+
+    public function UpdateMultiImage(Request $request)
+    {
+        $multi_image_id = $request->id;
+
+        if ($request->file('multi_image')) {
+            $multiImage = MultiImage::findOrFail($multi_image_id);
+            $img = $multiImage->multi_image;
+            $image = $request->file('multi_image');
+            unlink($img);
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(220, 220)->save('upload/multi/' . $name_gen);
+            $save_url = 'upload/multi/' . $name_gen;
+            MultiImage::findOrFail($multi_image_id)->update([
+                'multi_image' => $save_url,
+            ]);
+        }
+        $notification = array(
+            'message' => 'Multi Image Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.multi.image')->with($notification);
+    }
+
+    public function DeleteMultiImage($id)
+    {
+        $multiImage = MultiImage::findOrFail($id);
+        $img = $multiImage->multi_image;
+        unlink($img);
+        MultiImage::findOrFail($id)->delete();
+        $notification = array(
+            'message' => 'Multi Image Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.multi.image')->with($notification);
+    }
 }
+
