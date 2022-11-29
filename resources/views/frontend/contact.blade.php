33d559be69e88d8a807f5365664c8f5d7ca6dccd
Danylo <danilo.savchenko96@gmail.com>
2022-11-29 18:55:52 +0200

    Logo Uploads

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -11,7 +11,7 @@
                 <div class="row justify-content-center">
                     <div class="col-xl-6 col-lg-8 col-md-10">
                         <div class="breadcrumb__wrap__content">
-                            <h2 class="title">Contact us</h2>
+                            <h2 class="title">Contact me</h2>
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="/">Home</a></li>
