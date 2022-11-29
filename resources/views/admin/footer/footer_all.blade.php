ad6d475d81eefa076eb16f8a4fe78260b72eb139
Danylo <danilo.savchenko96@gmail.com>
2022-11-29 12:10:09 +0200

    Footer components with Frontend

---
+++ resources/views/admin/footer/footer_all.blade.php
@@ -0,0 +1,98 @@
+@extends('admin.admin_master')
+@section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Footer Page</h4>
+                            <form method="post" action="{{route('update.footer')}}">
+                                @csrf
+
+                                <input type="hidden" name="id" value="{{$footerData->id}}">
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Number</label>
+                                    <div class="col-sm-10">
+                                        <input name="number" class="form-control" type="text"
+                                               placeholder="Artisanal kale"
+                                               value="{{$footerData->number}}" id="example-text-input">
+                                    </div>
+                                </div>
+
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Short Description</label>
+                                    <div class="col-sm-10">
+                                        <textarea name="short_description" class="form-control" rows="5">{{$footerData->short_description}}</textarea>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Address</label>
+                                    <div class="col-sm-10">
+                                        <input name="address" class="form-control" type="text"
+                                               placeholder="Artisanal kale"
+                                               value="{{$footerData->address}}" id="example-text-input">
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input name="email" class="form-control" type="email"
+                                               placeholder="Artisanal kale"
+                                               value="{{$footerData->email}}" id="example-text-input">
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Facebook</label>
+                                    <div class="col-sm-10">
+                                        <input name="facebook" class="form-control" type="text"
+                                               placeholder="Artisanal kale"
+                                               value="{{$footerData->facebook}}" id="example-text-input">
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Twitter</label>
+                                    <div class="col-sm-10">
+                                        <input name="twitter" class="form-control" type="text"
+                                               placeholder="Artisanal kale"
+                                               value="{{$footerData->twitter}}" id="example-text-input">
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Copyright</label>
+                                    <div class="col-sm-10">
+                                        <input name="copyright" class="form-control" type="text"
+                                               placeholder="Artisanal kale"
+                                               value="{{$footerData->copyright}}" id="example-text-input">
+                                    </div>
+                                </div>
+
+                                <input type="submit" class="btn btn-info waves-effect waves-light"
+                                       value="Update Footer">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#image').change(function (e) {
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            });
+        });
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\BlogCategoryController;
+use App\Http\Controllers\Home\FooterController;
 use App\Http\Controllers\Home\PortfolioController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
@@ -96,5 +97,12 @@ Route::controller(BlogController::class)->group(function () {
     Route::get('/category/blog/{id}', 'CategoryBlog')->name('category.blog');
     Route::get('/blog', 'HomeBlog')->name('home.blog');
 });
+
+// Footer All Route
+Route::controller(FooterController::class)->group(function () {
+    Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
+    Route::post('/update/footer', 'UpdateFooter')->name('update.footer');
+});
+
 
 require __DIR__ . '/auth.php';
