89616aaaaacfd92a58c6460c733fcdeb7d7f11c9
Danylo <danilo.savchenko96@gmail.com>
2022-12-01 15:08:21 +0200

    Update middleware routes

--- routes/web.php
+++ routes/web.php
@@ -49,38 +49,42 @@ Route::middleware(['auth'])->controller(AdminController::class)->group(function 
 });
 
 // Home Slide All Route
-Route::controller(HomeSliderController::class)->group(function () {
+Route::middleware(['auth'])->controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
     Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 });
 
 // About Page All Route
 Route::controller(AboutController::class)->group(function () {
-    Route::get('/about/page', 'AboutPage')->name('about.page');
-    Route::post('/update/about', 'UpdateAbout')->name('update.about');
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/about/page', 'AboutPage')->name('about.page');
+        Route::post('/update/about', 'UpdateAbout')->name('update.about');
+        Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
+        Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
+        Route::get('/all/multi/image', 'AllMultiImage')->name('all.multi.image');
+        Route::get('/edit/multi/image/{id}', 'EditMultiImage')->name('edit.multi.image');
+        Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
+        Route::get('/delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image');
+    });
     Route::get('/about', 'HomeAbout')->name('home.about');
-    Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
-    Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
-    Route::get('/all/multi/image', 'AllMultiImage')->name('all.multi.image');
-    Route::get('/edit/multi/image/{id}', 'EditMultiImage')->name('edit.multi.image');
-    Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
-    Route::get('/delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image');
 });
 
 // Portfolio Page All Route
 Route::controller(PortfolioController::class)->group(function () {
-    Route::get('/all/portfolio', 'AllPortfolio')->name('all.portfolio');
-    Route::get('/add/portfolio', 'AddPortfolio')->name('add.portfolio');
-    Route::post('/store/portfolio', 'StorePortfolio')->name('store.portfolio');
-    Route::get('/edit/portfolio/{id}', 'EditPortfolio')->name('edit.portfolio');
-    Route::post('/update/portfolio', 'UpdatePortfolio')->name('update.portfolio');
-    Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
-    Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/all/portfolio', 'AllPortfolio')->name('all.portfolio');
+        Route::get('/add/portfolio', 'AddPortfolio')->name('add.portfolio');
+        Route::post('/store/portfolio', 'StorePortfolio')->name('store.portfolio');
+        Route::get('/edit/portfolio/{id}', 'EditPortfolio')->name('edit.portfolio');
+        Route::post('/update/portfolio', 'UpdatePortfolio')->name('update.portfolio');
+        Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
+    });
     Route::get('/portfolio', 'HomePortfolio')->name('home.portfolio');
+    Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
 });
 
 // Blog Category All Route
-Route::controller(BlogCategoryController::class)->group(function () {
+Route::middleware(['auth'])->controller(BlogCategoryController::class)->group(function () {
     Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
     Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
     Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category');
@@ -91,33 +95,37 @@ Route::controller(BlogCategoryController::class)->group(function () {
 
 // Blog All Route
 Route::controller(BlogController::class)->group(function () {
-    Route::get('/all/blog', 'AllBlog')->name('all.blog');
-    Route::get('/add/blog', 'AddBlog')->name('add.blog');
-    Route::post('/store/blog', 'StoreBlog')->name('store.blog');
-    Route::get('/edit/blog/{id}', 'EditBlog')->name('edit.blog');
-    Route::post('/update/blog', 'UpdateBlog')->name('update.blog');
-    Route::get('/delete/blog/{id}', 'DeleteBlog')->name('delete.blog');
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/all/blog', 'AllBlog')->name('all.blog');
+        Route::get('/add/blog', 'AddBlog')->name('add.blog');
+        Route::post('/store/blog', 'StoreBlog')->name('store.blog');
+        Route::get('/edit/blog/{id}', 'EditBlog')->name('edit.blog');
+        Route::post('/update/blog', 'UpdateBlog')->name('update.blog');
+        Route::get('/delete/blog/{id}', 'DeleteBlog')->name('delete.blog');
+    });
     Route::get('/blog/details/{id}', 'BlogDetails')->name('blog.details');
     Route::get('/category/blog/{id}', 'CategoryBlog')->name('category.blog');
     Route::get('/blog', 'HomeBlog')->name('home.blog');
 });
 
 // Footer All Route
-Route::controller(FooterController::class)->group(function () {
+Route::middleware(['auth'])->controller(FooterController::class)->group(function () {
     Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
     Route::post('/update/footer', 'UpdateFooter')->name('update.footer');
 });
 
 // Contact All Route
 Route::controller(ContactController::class)->group(function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/delete/message/{id}', 'DeleteMessage')->name('delete.message');
+        Route::get('/contact/message', 'ContactMessage')->name('contact.message');
+    });
     Route::get('/contact', 'Contact')->name('contact.me');
     Route::post('/store/message', 'StoreMessage')->name('store.message');
-    Route::get('/contact/message', 'ContactMessage')->name('contact.message');
-    Route::get('/delete/message/{id}', 'DeleteMessage')->name('delete.message');
 });
 
 // ClientFeedback All Route
-Route::controller(ClientFeedbackController::class)->group(function () {
+Route::middleware(['auth'])->controller(ClientFeedbackController::class)->group(function () {
     Route::get('/all/feedback', 'AllFeedback')->name('all.feedback');
     Route::get('/add/feedback', 'AddFeedback')->name('add.feedback');
     Route::post('/store/feedback', 'StoreFeedback')->name('store.feedback');
